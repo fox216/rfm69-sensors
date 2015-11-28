0d01f274f1e7a8a706622ce3380077cd8d2251c9
kevin fox <kevin.j.fox@gmail.com>
2015-11-28 10:25:17 -0500

    Adding LED sensor

---
+++ rfm69-led/NodeConf.h
@@ -0,0 +1,31 @@
+// !!!!  DEVICE CONFIGURATION SETTINGS  !!!!
+#ifndef NodeConf_h
+#define NodeConf_h
+
+#if (ARDUINO < 100)
+#include "WProgram.h"
+#else
+#include <Arduino.h>
+#endif
+
+/*---------------------------------------------
+|
+|	Node Configuration Settings
+|	
+*/
+
+#define NODEID      42
+#define NETWORKID   50
+#define FREQUENCY   RF69_915MHZ //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
+#define KEY         "thisIsEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
+#define LED         9
+#define SERIAL_BAUD 115200
+#define ACK_TIME    30  // # of ms to wait for an ack
+#define WAIT_TIME   1000
+//#define TRANSMITPERIOD 10000 // 10 Seconds
+//#define TRANSMITPERIOD 120000 // 2 min
+//#define TRANSMITPERIOD 300000 // 5 min
+#define TRANSMITPERIOD 600000 // 10 min
+#define GET_SPKL_STATUS 35000 
+
+#endif
---
+++ rfm69-led/rfm69-led.cc
@@ -0,0 +1,56 @@
+#include <RFM69.h>
+#include <SPI.h>
+#include <NodeMsg.h> // Load Gateway Message structure
+#include <NodeConf.h>
+// declare radio
+RFM69 radio;
+bool wd_active = false;
+bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
+
+
+void setup() {
+	delay(10);
+	radio.initialize(FREQUENCY,NODEID,NETWORKID);
+	radio.encrypt(KEY);
+	radio.promiscuous(promiscuousMode);
+
+}
+
+void blink(byte PIN, int DELAY_MS) {
+	pinMode(PIN, OUTPUT);
+	//times = times * 2;
+	digitalWrite(PIN, HIGH);
+	delay(DELAY_MS);
+	digitalWrite(PIN, LOW);
+}
+
+void LedState(byte PIN, byte state) {
+	pinMode(PIN, OUTPUT);
+	if (state > 0) {
+		digitalWrite(PIN, HIGH);
+	} else {
+		digitalWrite(PIN, LOW);
+	}
+}
+
+
+void loop() {
+
+	// Process message from Gateway...
+	if ( radio.receiveDone() ) {
+		//blink(LED,5);
+		payload = *(Payload*)radio.DATA;
+		switch(payload.MsgType) {
+			// Find matching message handler
+			case LedMsg:
+				i_LedMsg = *(I_LedMsg*)payload.msg;
+				LedState(i_LedMsg.led, i_LedMsg.state);
+			default:
+				blink(LED, 5);
+		}
+	}
+}
+
+
+
+
