19582a4dc2c9c3aca9e20ee40f7f6024dd399594
kevin fox <kevin.j.fox@gmail.com>
2016-02-27 16:13:45 -0500

    Adding message types for Sprinkler control sensor

--- rfm69-sprkl-ctrl/NodeMsg.h
+++ rfm69-sprkl-ctrl/NodeMsg.h
@@ -21,6 +21,9 @@
 */
 
 
+
+
+
 typedef enum {
 	zoneCtrl 		= 10,    	// Control individual Zone
 	zoneGroupCtrl 	= 20, 		// Control predefined groups of Zones
@@ -34,10 +37,21 @@ typedef struct {
 } Payload;
 Payload payload;
 
+/*
+	Features
+	------------------------------------------------
+	zoneCtrl: 	Control single zone. Turn on single zone for default run time. 
+	runProg:	Run predefined sprinkle program based on control character (Example: Front[f], Back[b])
+	sysCtrl:	Pause / Resume system.
+	- Not Implemented sysStatus:	Query System status.
+	sysConfig:	Set system configuration vales [EPROM]. (Zone run time, Dwell time[Pause time between zones])
+*/
+
 
 typedef struct {
 	// _zoneCtrl
 	// MESSAGE TYPE = 10
+	// MSG_SIZE = 2
 	byte	zone;				// Zone to activate
 	byte	cycleSelect;		// length of time zone should run (Predefined times)
 } _zoneCtrl;
@@ -48,12 +62,30 @@ _zoneCtrl o_zoneCtrl; 			// outbound message
 typedef struct { 
 	// _SprklMsg
 	// MESSAGE TYPE = 
+	// MSG_SIZE = 2 
 	byte 		zone; 			// Sprinkler Zone to control
 	byte		state;			// Duration of sprinkle time
 } _SprklMsg;
 _SprklMsg	i_SprklMsg;
 _SprklMsg 	o_SprklMsg;
 
+tyepdef struct {
+	// _SysStatus
+	// MESSAGE Type = 40
+	// MSG_SIZE = 
+	byte 	zone;  				// Current Zone
+	char 	state;				// System State: (R: Run, P: Paused, I: Idle, C: Cancel) 
+	byte	runTime;			// Zone run time
+	byte	dwellTime;			// Wait time between Zone Switch
+	byte	percComplete;		// Percent complete (Range: 0 - 100) 
+} _SysCtrl;
+_SysStatus	i_SysStatus;
+_SysStatus	o_SysStatus;
+
+tyepdef struct {
+	char 	program;			// Predefined set of zones in sequence
+} _runProg;
+_runProg	i_runProg;
 
 #endif
 
