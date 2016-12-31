154fa3ef03cc251ab40b2e80e13dded232b978e5
Kevin Fox <kevin.j.fox@gmail.com>
2016-12-31 15:10:35 -0500

    Adding heartbeat sensor

--- rfm-led2/Sensors.h
+++ rfm-led2/Sensors.h
@@ -10,6 +10,7 @@
 
 #define LED1 3
 #define LED2 16
+#define SENSOR_HEART_BEAT 5000 // 5 seconds
 
 typedef struct {
   byte			m1;
@@ -32,5 +33,13 @@ typedef struct {
   byte      led2;
 } _Sensor_t5;
 _Sensor_t5 Sensor_t5;
+
+typedef struct {
+  byte              m1;
+  unsigned long     runCount;
+  byte              m2;
+  unsigned long     MillSecs;
+} _Sensor_t100;
+_Sensor_t100 Sensor_t100;
 
 #endif
